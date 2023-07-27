9564e2c185453ea41559e9b24130a54342c8e35a
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-07-27 13:56:48 -0300

    Update 1061.java

--- Beecrowd/1061.java
+++ Beecrowd/1061.java
@@ -1,53 +1,50 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Scanner tc = new Scanner(System.in);
-            int[] dia = new int[2];
-            int[] hora = new int[2];
-            int[] min = new int[2];
-            int[] seg = new int[2];
+    public class Main {
+        public static void main(String[] args) {
+            try {
+                Scanner tc = new Scanner(System.in);
+                int[] dia = new int[2];
+                int[] hora = new int[2];
+                int[] min = new int[2];
+                int[] seg = new int[2];
 
-            tc.next();
-            dia[0] = Integer.parseInt(tc.next());
-            hora[0] = Integer.parseInt(tc.next());
-            tc.next();
-            min[0] = Integer.parseInt(tc.next());
-            tc.next();
-            seg[0] = Integer.parseInt(tc.next());
+                tc.next();
+                dia[0] = Integer.parseInt(tc.next());
+                hora[0] = Integer.parseInt(tc.next());
+                tc.next();
+                min[0] = Integer.parseInt(tc.next());
+                tc.next();
+                seg[0] = Integer.parseInt(tc.next());
 
-            tc.next();
-            dia[1] = Integer.parseInt(tc.next());
-            hora[1] = Integer.parseInt(tc.next());
-            tc.next();
-            min[1] = Integer.parseInt(tc.next());
-            tc.next();
-            seg[1] = Integer.parseInt(tc.next());
+                tc.next();
+                dia[1] = Integer.parseInt(tc.next());
+                hora[1] = Integer.parseInt(tc.next());
+                tc.next();
+                min[1] = Integer.parseInt(tc.next());
+                tc.next();
+                seg[1] = Integer.parseInt(tc.next());
 
-            int diaf = dia[1]-dia[0];
-            int segf, horaf, minf;
-            int adiciona=0;
-            if (seg[1] >= seg[0])
-                segf = seg[1]-seg[0];
-            else
-                segf = 60 + seg[1] - seg[0];
+                long[] segundosTotal = new long[2];
+                segundosTotal[0] = (dia[0]*24*60*60)+(hora[0]*60*60)+(min[0]*60)+seg[0];
+                segundosTotal[1] = (dia[1]*24*60*60)+(hora[1]*60*60)+(min[1]*60)+seg[1];;
 
-            if (min[1] >= min[0])
-                minf = min[1] - min[0];
-            else
-                minf = 60 + min[1] - min[0];
+                segundosTotal[0] = segundosTotal[1] - segundosTotal[0];
+                min[0] = (int)segundosTotal[0]/60;
+                seg[0] = (int)segundosTotal[0]%60;
 
-            if (hora[1] > hora[0])
-                horaf = hora[1] - hora[0];
-            else
-                horaf = 60 + hora[1] - hora[0];
+                hora[0] = min[0]/60;
+                min[0] = min[0]%60;
 
-            System.out.println(diaf + " dia(s)\n" + horaf + " hora(s)\n" +
-                    minf + " minuto(s)\n" + segf + "segundo(s)");
+                dia[0] = hora[0]/24;
+                hora[0] = hora[0]%24;
 
-        } catch (Exception NumberFormatException) {
-            System.exit(1);
-        }
+
+                System.out.println(dia[0] + " dia (s)\n" + hora[0] + " hora (s)\n" +
+                        min[0] + " minuto (s)\n" + seg[0] + " segundo (s)");
+
+            } catch (Exception NumberFormatException) {
+                System.exit(1);
+            }
         }
     }
