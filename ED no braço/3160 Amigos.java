c5e5ebdcfb756f4e6ece01087c56efba04272247
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-08-11 10:15:54 -0300

    Update 3160 Amigos.java

--- ED no braço/3160 Amigos.java
+++ ED no braço/3160 Amigos.java
@@ -1,116 +1,108 @@
 import java.util.Scanner;
 
 class Main{
-    public static void main (String[] args){
-        Scanner tc = new Scanner(System.in);
-        String pegaTudo = tc.nextLine();
-        Lista atual = new Lista();
-        String compara = pegaTudo;
+   public static void main (String[] args){
+      Scanner tc = new Scanner(System.in);
+      Lista tudo = new Lista();
+      String linha;
 
-        int i = 0;
-        while (true) {
-            if (compara.length() < i)
-                i = 0;
-            else if (compara.length() == 1)
-                break;
-            if (compara.charAt(i) == ' ') {
-                atual.adiciona(compara.substring(0, i));
-                compara = compara.substring(i);
-                System.out.println(compara);
-            }
-            i++;
-        }
-        pegaTudo = tc.nextLine();
-        Lista nova = new Lista();
-        compara = pegaTudo;
+      for (int i = 0; i < 2; i++){
+         linha = tc.nextLine();
+         tudo.adicionaString(linha.split(" "));
+      }
 
-//        while (true) {
-//            if (compara.length() <= i)
-//                i = 0;
-//            else if (compara.length() == 1)
-//                break;
-//            if (compara.charAt(i) == ' ') {
-//                atual.adiciona(compara.substring(0, i));
-//                compara = compara.substring(i);
-//                System.out.println(compara);
-//            }
-//            i++;
-//        }
-//
-//        pegaTudo = tc.nextLine();
-//            atual.printa(); System.out.println();
-//            nova.printa();
+      linha = tc.nextLine();
 
-    }
+      if (linha.equalsIgnoreCase("nao"))
+         tudo.printa();
+      else {
+         for (int i = 1; i <= tudo.getTam(); i++) {
+            if (tudo.retorna(i).equals(linha))
+               tudo.printa();
+            System.out.print(tudo.retorna(i) + " ");
+         }
+      }
+   }
 }
 
 class Lista{
-    private No cabeca;
-    private int tam;
-    private No ultimo;
+   private No cabeca;
+   private int tam;
+   private No ultimo;
 
-    public Lista(){
-        cabeca = null;
-        tam = 0;
-        ultimo = null;
-    }
+   public Lista(){
+      cabeca = null;
+      tam = 0;
+      ultimo = null;
+   }
 
-    public String retorna(){
-        return cabeca.getNome();
-    }
-    public void printa(){
-        No aux = cabeca;
-        if (verifica())
-            return;
-        else
-            for (int i = 1; i <= tam; i++) {
-                System.out.print(aux.getNome() + " ");
-                aux = aux.getProx();
-            }
-    }
+   public String retorna(int indice){
+      No aux = cabeca;
+      if (indice > tam || verifica() || indice <= 0)
+         return "";
+      for (int i = 1; i < indice; i++)
+         aux = aux.getProx();
 
-    public void adiciona(String nome){
-        No novoNo = new No(nome);
-        if (verifica()){
+      return aux.getNome();
+   }
+   public void printa(){
+      No aux = cabeca;
+      if (verifica())
+         return;
+      else
+         for (int i = 1; i <= tam; i++) {
+            System.out.print(aux.getNome() + " ");
+            aux = aux.getProx();
+         }
+   }
+
+   public void adicionaString(String[] nome){
+      for (int i = 0; i < nome.length; i++) {
+         No novoNo = new No(nome[i]);
+         if (verifica()) {
             novoNo.setProx(cabeca);
             cabeca = novoNo;
             ultimo = novoNo;
             //System.out.println(cabeca.getNome());
             tam++;
-        } else {
+         } else {
             ultimo.setProx(novoNo);
             ultimo = novoNo;
             //System.out.println(ultimo.getNome());
             tam++;
-        }
+         }
+      }
+   }
 
-    }
+   public boolean verifica(){
+      return (tam==0);
+   }
 
-    public boolean verifica(){
-        return (tam==0);
-    }
+   public int getTam() {
+      return tam;
+   }
 }
 
 
 
 class No {
-    private String nome;
-    private No prox;
+   private String nome;
+   private No prox;
 
-    public No (String nome){
-        this.nome = nome;
-        prox = null;
-    }
+   public No (String nome){
+      this.nome = nome;
+      prox = null;
+   }
 
-    public void setProx(No prox){
-        this.prox = prox;
-    }
+   public void setProx(No prox){
+      this.prox = prox;
+   }
 
-    public String getNome(){
-        return nome;
-    }
+   public String getNome(){
+      return nome;
+   }
 
-    public No getProx() {
-        return prox;
-    }
+   public No getProx() {
+      return prox;
+   }
 }
