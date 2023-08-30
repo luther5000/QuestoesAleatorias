8b0e60f0c3cdbb3fd41c0f903532efb878ac22cf
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-08-30 13:53:22 -0300

    Update 1119 - A Fila de Desempregados.java

--- ED no braço/1119 - A Fila de Desempregados.java
+++ ED no braço/1119 - A Fila de Desempregados.java
@@ -12,13 +12,12 @@ public class Main {
         while (!(n == 0 && k == 0 && m ==0)) {
 
             Lista lista = new Lista(n);
-            int[] vet;
+
 
 
             while (!lista.vazia()) {
-                vet = lista.remove(k, m);
-
-
+                int[] vet = lista.remove(k, m);
+                lista.printa();
                 if (vet.length == 1)
                     System.out.print(" " + vet[0] + ",");
                 else
@@ -39,49 +38,124 @@ public class Main {
 }
 
 class Lista {
-    private int[] vet;
-    private int tamMax;
     private int tamAtual;
+    private No inicio;
+    private No fim;
+    private int pessoaHorarioAtual;
+    private int pessoaAntiHorarioAtual;
 
 
     public Lista (int quant){
-        vet = new int[quant];
-        tamMax = quant;
-        tamAtual = tamMax;
+        tamAtual = quant;
+        pessoaHorarioAtual = 1;
+        pessoaAntiHorarioAtual = 1;
+        adiciona(quant);
+    }
+
+    public void adiciona(int quant){
+        int cont = 1;
+        while (cont <= quant){
+            No novoNo = new No(cont);
+            if (cont == 1){
+                inicio = novoNo;
+                fim = novoNo;
+            } else {
+                fim.setProx(novoNo);
+                novoNo.setAnt(fim);
+                fim = novoNo;
+                novoNo.setProx(inicio);
+                inicio.setAnt(novoNo);
+            }
+            ++cont;
+        }
     }
 
     public int[] remove(int horario, int antiHorario){
-        if (this.vet[horario-1] == 0 && this.vet[tamMax - antiHorario] == 0) {
-            if (horario - 1 == tamMax - antiHorario) {
-                this.vet[horario - 1] = 1;
-                int[] vet = {horario - 1};
-                --tamAtual;
-                return vet;
-            } else {
-                this.vet[horario - 1] = 1;
-                this.vet[tamMax - antiHorario] = 1;
-                int[] vet = {horario - 1, tamMax - antiHorario};
-                --tamAtual;
-                return vet;
-            }
-        }else if (this.vet[horario - 1] == 0){
-            this.vet[horario - 1] = 1;
-            int[] vet = {horario - 1};
+        No aux1 = pessoaAtual(pessoaHorarioAtual, 1);
+        for (int i = 1; i < horario + pessoaHorarioAtual; i++){
+            aux1 = aux1.getProx();
+        }
+
+        No aux2 = fim;
+        for (int i = 1; i < antiHorario + pessoaAntiHorarioAtual; i++)
+            aux2 = aux2.getAnt();
+
+        pessoaHorarioAtual = aux1.getNum();
+        pessoaAntiHorarioAtual = aux2.getNum();
+
+        if (pessoaHorarioAtual == pessoaAntiHorarioAtual){
+            int[] vet = {pessoaHorarioAtual};
+            aux1.getAnt().setProx(aux1.getProx());
+            aux1.getProx().setAnt(aux1.getAnt());
             --tamAtual;
             return vet;
-        } else if (this.vet[tamMax - antiHorario] == 0){
-            this.vet[tamMax - antiHorario] = 1;
-            int[] vet = {tamMax - antiHorario};
-            --tamAtual;
-            return vet;
-        } else {
-            int[] vet = {};
-            --tamAtual;
+        } else{
+            aux1.getAnt().setProx(aux1.getProx());
+            aux1.getProx().setAnt(aux1.getAnt());
+            aux2.getAnt().setProx(aux2.getProx());
+            aux2.getProx().setAnt(aux2.getAnt());
+            int[] vet = {pessoaHorarioAtual, pessoaAntiHorarioAtual};
+            tamAtual -= 2;
             return vet;
         }
     }
 
+    public No pessoaAtual(int indice, int qual){
+        No aux = inicio;
+        if (qual == 1)
+            while (aux.getNum() != indice)
+                aux = aux.getProx();
+        else
+            while (aux.getNum() != indice)
+                aux = aux.getAnt();
+
+        return aux;
+    }
+
+
+
+    public void printa(){
+        No aux = inicio;
+        for (int i = 0; i < tamAtual; i++) {
+            System.out.print(aux.getNum() + " ");
+            aux = aux.getProx();
+        }
+        System.out.println();
+    }
+
     public boolean vazia(){
         return tamAtual==0;
+    }
+}
+
+class No{
+    private int num;
+    private No prox;
+    private No ant;
+
+    public No(int num){
+        this.num = num;
+        prox = null;
+        ant = null;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public No getAnt() {
+        return ant;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setAnt(No ant) {
+        this.ant = ant;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
     }
 }
