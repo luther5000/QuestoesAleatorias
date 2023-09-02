46a11a3967f4161a681d12a9a8ad3572d2136746
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-09-02 20:29:55 -0300

    Update 1031 - Crise de Energia.java

--- ED no braço/1031 - Crise de Energia.java
+++ ED no braço/1031 - Crise de Energia.java
@@ -1,36 +1,41 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 class Main {
     public static void main (String[] args){
         Scanner tc = new Scanner(System.in);
         int tam = Integer.parseInt(tc.nextLine());
+        
         while (tam != 0) {
             Lista l = new Lista(tam);
+            
             int res = fazTudo(l, tam);
             System.out.println(res);
+            
             tam = Integer.parseInt(tc.nextLine());
         }
     }
 
     public static int fazTudo(Lista l, int tam){
         int a = 1;
+        
         while (!(l.remove(a))){
-            System.out.println(a);
             l = new Lista(tam);
             ++a;
         }
+        
         return a;
     }
 }
 
 class Lista {
     private No cabeca;
+    private No fim;
     private int tam;
     private No atual;
 
     public Lista(int tam) {
         adiciona(tam);
+        
         this.tam = tam;
         atual = cabeca;
     }
@@ -38,34 +43,49 @@ class Lista {
     public void adiciona(int tam) {
         No aux = new No(1);
         cabeca = aux;
+        
         for (int i = 2; i <= tam; i++) {
             No aux2 = new No(i);
+            
             aux.setProx(aux2);
             aux = aux2;
         }
+        
         aux.setProx(cabeca);
+        fim = aux;
     }
 
-   public boolean remove (int pulos){
+    public boolean remove (int pulos){
         No aux = atual;
-        No aux2  = aux;
+        No aux2  = fim;
+        
+        aux2.setProx(aux.getProx());
+        atual = aux.getProx();
+        aux = aux.getProx();
+        --tam;
+        
         while (!(vazia())) {
-            for (int i = 1; i <= pulos; i++) {
+            for (int i = 1; i < pulos; i++) {
                 aux2 = aux;
                 aux = aux.getProx();
             }
+            
             aux2.setProx(aux.getProx());
+            atual = aux.getProx();
             --tam;
+            
             if (!(vazia()) && aux.getRegiao() == 13){
                 return false;
             }
+            
+            aux = atual;
         }
         return true;
-   }
-
-   public boolean vazia(){
+    }
+    
+    public boolean vazia(){
         return tam==0;
-   }
+    }
 }
 
 
