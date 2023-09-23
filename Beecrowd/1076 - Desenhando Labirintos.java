fdda4b93cc0cc2784c8ab2a65377694d156c2cd4
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-09-22 23:23:18 -0300

    Update 1076 - Desenhando Labirintos.java

--- Beecrowd/1076 - Desenhando Labirintos.java
+++ Beecrowd/1076 - Desenhando Labirintos.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import static java.lang.Math.*;
 
 class Main {
 
@@ -21,9 +20,9 @@ class Main {
             Grafo grafo = new Grafo(vertices);
 
             grafo.adiciona(vet);
-            int quant = grafo.verifica(inicio);
+            int quant = grafo.dfs(inicio);
 
-            System.out.println(quant);
+            System.out.println(2*quant);
 
             --casos;
         }
@@ -33,42 +32,26 @@ class Main {
 class Grafo {
     private int tam;
     private List<Integer>[] adjacencia;
-    private int[] pai;
 
     public Grafo(int tam){
         adjacencia = new List[tam];
         this.tam = tam;
-        pai = new int[tam];
 
         for (int i = 0; i < tam; i++) {
             adjacencia[i] = new ArrayList<>();
-            pai[i] = -1;
         }
     }
 
     public void adiciona(int[] vertices){
         for (int i = 0; i < vertices.length; i += 2){
-            adjacencia[vertices[i]].add(vertices[i+1]);
-            adjacencia[vertices[i+1]].add(vertices[i]);
+            if(!(adjacencia[vertices[i]].contains(vertices[i+1]))) {
+                adjacencia[vertices[i]].add(vertices[i + 1]);
+                adjacencia[vertices[i + 1]].add(vertices[i]);
+            }
         }
     }
 
-    public int verifica(int inicio){
-        int ret = dfs (inicio);
-        for (int i = 0; i < tam; i++)
-            System.out.print(pai[i] + " ");
-
-        int cont = 0;
-        for (int i = 0; i < tam; i++)
-            if (pai[i] == -1)
-                ++cont;
-        if (cont == 1)
-            return ret;
-        else
-            return -1;
-    }
-
-    private int dfs(int inicio){
+    public int dfs(int inicio){
         boolean[] flag = new boolean[tam];
         Stack<Integer> pilha = new Stack<>();
 
@@ -79,17 +62,15 @@ class Grafo {
             if (pilha.isEmpty() && !flag[j]){
                 pilha.push(j);
                 flag[j] = true;
-                ++contador;
             }
 
             while (!(pilha.isEmpty())) {
                 int ant = pilha.pop();
                 for (int i = 0; i < adjacencia[ant].size(); i++) {
-                    ++contador;
                     if (!(flag[adjacencia[ant].get(i)])) {
-                        pai[adjacencia[ant].get(i)] = ant;
                         flag[adjacencia[ant].get(i)] = true;
                         pilha.push(adjacencia[ant].get(i));
+                        ++contador;
                     }
                 }
             }
