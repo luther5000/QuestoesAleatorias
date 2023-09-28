c7db873bce913cc6d63eaded27405e7dce6ef373
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-09-28 13:55:10 -0300

    Update 3065 - Calculando.java

--- Beecrowd/3065 - Calculando.java
+++ Beecrowd/3065 - Calculando.java
@@ -5,34 +5,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
         int numOps = Integer.parseInt(tc.nextLine());
+        int teste = 1;
 
         while (numOps != 0){
             Queue<Integer> fila = new LinkedList<>();
             String s = tc.nextLine();
-            char[] ops = new char[numOps];
-            int j = 0;
             int num = 0;
-
             System.out.println(s.length());
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '+' || s.charAt(i) == '-') {
                     fila.add(Integer.parseInt(s.substring(0, i)));
-                    ops[j++] = s.charAt(i);
 
-                    s = s.substring(0, i);
+                    s = s.substring(i);
                     i = 0;
-                    System.out.println("entrou");
+                    ++num;
+                    if (num+1 == numOps){
+                        fila.add(Integer.parseInt(s));
+                        break;
+                    }
                 }
+            }
+            
+            if (fila.isEmpty()){
+                numOps = Integer.parseInt(tc.nextLine());
+                continue;
             }
 
             int total = fila.remove();
-            for (int i = 0; i < ops.length; i++)
-                if (ops[i] == '+')
-                    total += fila.remove();
-                else
-                    total -= fila.remove();
+            while (!(fila.isEmpty())) {
+                total += fila.remove();
+            }
 
+            System.out.println("Teste " + teste + "\n" + total);
             numOps = Integer.parseInt(tc.nextLine());
+            ++teste;
         }
     }
 }
