d734841a82e39fed5665c1cc297ef6e69672bc4e
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-10-03 13:59:02 -0300

    Update 1835 Promessa de Campanha.java

--- Beecrowd/1835 Promessa de Campanha.java
+++ Beecrowd/1835 Promessa de Campanha.java
@@ -8,8 +8,25 @@ public class Main {
         int quantos = 1;
 
         while (casos > 0){
-            int tam = Integer.parseInt(tc.nextLine());
-            int[] estradas = new int[Integer.parseInt(tc.nextLine())*2];
+            String entradaBugada = tc.nextLine();
+            int tam;
+            int[] estradas;
+            if (entradaBugada.contains(" ")) {
+                String[] bug = entradaBugada.split(" ");
+                tam = Integer.parseInt(bug[0]);
+                estradas = new int[Integer.parseInt(bug[1])*2];
+            } else {
+                tam = Integer.parseInt(entradaBugada);
+                estradas = new int[Integer.parseInt(tc.nextLine())*2];
+            }
+
+            if (estradas.length == 0) {
+                System.out.println("Caso #" + quantos + ": ainda falta(m) " + (tam-1) + " estrada(s)");
+                ++quantos;
+                --casos;
+                continue;
+            }
+
             for (int i = 0; i < estradas.length; i += 2){
                 estradas[i] = Integer.parseInt(tc.next())-1;
                 estradas[i+1] = Integer.parseInt(tc.next())-1;
@@ -66,9 +83,9 @@ class Grafo{
             while (!(pilha.isEmpty())){
                 atual = pilha.pop();
                 for (int j = 0; j < lista[atual].size(); j++)
-                    if (!(flag[lista[atual].get(i)])){
-                        flag[lista[atual].get(i)] = true;
-                        pilha.push(lista[atual].get(i));
+                    if (!(flag[lista[atual].get(j)])){
+                        flag[lista[atual].get(j)] = true;
+                        pilha.push(lista[atual].get(j));
                     }
             }
         }
