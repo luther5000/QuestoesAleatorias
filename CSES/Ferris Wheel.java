a3bef46847d6578ac3e6199a38ce5c55bfa8d82b
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-10-05 13:19:18 -0300

    Update Ferris Wheel.java

--- CSES/Ferris Wheel.java
+++ CSES/Ferris Wheel.java
@@ -1,46 +1,40 @@
 import java.util.*;
 
- public class Main{
+public class Main{
     public static void main(String[] args){
         Scanner tc = new Scanner(System.in);
         int tam = Integer.parseInt(tc.next());
-        List<Integer> alturas= new ArrayList<>();
+        int[] alturas = new int[tam];
         int alturaMax = Integer.parseInt(tc.next());
         tc.nextLine();
 
         for (int i = 0; i < tam; i++){
-            alturas.add(Integer.parseInt(tc.next()));
+            alturas[i] = Integer.parseInt(tc.next());
         }
 
-        alturas.sort(Comparator.naturalOrder());
+        Arrays.sort(alturas);
 
         int barcos = 0;
 
 
-        int j = 0;
-        while (!(alturas.isEmpty())){
-            if (alturas.size() == 1){
+        int j = alturas.length;
+        int i = 0;
+        while (true){
+            if (i == j ) {
                 ++barcos;
                 break;
             }
-            if (alturas.get((alturas.size()-1)/2) + alturas.get(alturas.size()-1) <= alturaMax)
-                if (alturas.size() % 2 == 0) {
-                    barcos += alturas.size() / 2;
-                    break;
-                }
-                else {
-                    barcos += alturas.size() / 2 + 1;
-                    break;
-                }
+            if (i > j)
+                break;
 
-            if (alturas.get(j) + alturas.get(alturas.size()-1) <= alturaMax) {
+            if (alturas[i] + alturas[j] <= alturaMax) {
                 ++barcos;
-                alturas.remove(j);
-                alturas.remove(alturas.size()-1);
+                --j;
+                ++i;
             }
             else {
                 ++barcos;
-                alturas.remove(alturas.size()-1);
+                --j;
             }
         }
 
