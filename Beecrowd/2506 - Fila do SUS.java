0f5cd334f04ab899c743f183d3d5e53d6300fd51
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-10-06 15:52:09 -0300

    Update 2506 - Fila do SUS.java

--- Beecrowd/2506 - Fila do SUS.java
+++ Beecrowd/2506 - Fila do SUS.java
@@ -4,37 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
-
+        int cont = 0;
         while (tc.hasNext()) {
+            if (cont != 0){
+                tc.nextLine();
+            }
             int casos = Integer.parseInt(tc.nextLine());
             Queue<Paciente> fila = new LinkedList<>();
 
             for (int i = 0; i < casos; i ++) {
-                int hora = Integer.parseInt(tc.next())*60 + Integer.parseInt(tc.next());
-                if (hora < i) {
-                    Paciente novo = new Paciente(hora, Integer.parseInt(tc.next()));
-                    fila.add(novo);
-                }
-                tc.nextLine();
+                Paciente novo = new Paciente(Integer.parseInt(tc.next())*60 + Integer.parseInt(tc.next()),
+                        Integer.parseInt(tc.next()));
+                fila.add(novo);
+                if (casos -1 != i)
+                    tc.nextLine();
             }
 
             int criticos = 0;
             int horaAtual = 7*60;
-            
-            while (!(fila.isEmpty()) && horaAtual <= 19*60) {
+            cont = 1;
+            while (!(fila.isEmpty())) {
                 if (horaAtual - fila.peek().hora < 0) {
                     horaAtual += 30;
                     continue;
                 }
 
-                if (horaAtual - fila.peek().hora > fila.peek().tempo) 
+                Paciente atual = fila.remove();
+
+                if (horaAtual - atual.hora > atual.tempo) {
                     ++criticos;
+                }
 
                 horaAtual += 30;
-                fila.remove();
             }
-
             System.out.println(criticos);
+
         }
     }
 }
